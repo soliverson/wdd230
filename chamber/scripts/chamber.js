7c1dcea04be7569888eb91948791f6a493a7cdcb
soliverson <stacy.oliverson@gmail.com>
2024-02-02 14:29:21 -0700

    updated chamber page

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,36 @@
+
+//declaring the date in the footer
+const now = new Date();
+const currentYear = now.getFullYear();
+let dateTime = document.lastModified;
+
+//declaring the current year in the footer
+const domCurrentYear = document.querySelector('#current-year');
+domCurrentYear.innerText = currentYear;
+
+//declaring last modified date on footer
+const lastModified = document.querySelector('#current-date');
+lastModified.innerText = dateTime
+
+//declaring navigation variables
+const hamburguerElement = document.querySelector("#menu");
+const navElements = document.querySelector(".navigation");
+
+//declaring dark mode variables
+const darkButton = document.getElementById('darkBtn');
+const main = document.querySelector('main')
+
+//adding class for open and close hamburguer menu
+hamburguerElement.addEventListener('click', () => {
+    navElements.classList.toggle('open');
+    hamburguerElement.classList.toggle('open');  
+})
+
+//code for dark mode click
+darkButton.addEventListener('click',() =>{
+    main.classList.toggle('dark');
+})
+
+// get .visits and initialize element
+const visitsElement = document.querySelector(".visits");
+
