c628549a902d48f814a3fa74b36c24226f8253ca
soliverson <stacy.oliverson@gmail.com>
2024-02-23 13:41:10 -0700

    updated bom

---
+++ lesson07/scripts/bom.js
@@ -0,0 +1,42 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+        displayList(input.value); 
+        chaptersArray.push(input.value);  
+        setChapterList(); 
+        input.value = ''; 
+        input.focus();
+    }
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    li.textContent = item; 
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete'); 
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent); 
+        input.focus(); 
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList')) || [];
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
