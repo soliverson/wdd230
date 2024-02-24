d687f0c72723159ced5aa14df92522b96f4e3682
soliverson <stacy.oliverson@gmail.com>
2024-02-24 10:43:40 -0700

    update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,32 +1,24 @@
-// get .visits and initialize element
-const visitsElement = document.querySelector(".visits");
+const messageElement = document.getElementById('message')
 
-// getting current day
-let currentDate = new Date();
-let today = Date.now();
-
-// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
-const msToDays = 84600000;
-
-// get the num of visits in the local storage if exists or (||) assign it to 0
-let lastVisit = Number(window.localStorage.getItem('lastVisitLocal')) || currentDate;
-//let lastVisit = new Date(lastVisitDay).getTime();
-
-//getting the time since last visit
-let daysSinceLastVisit = (today - lastVisit)/msToDays;
-
-
-//display the num of visits or a message if is the first visit of the user
-if(daysSinceLastVisit == 0){
-    visitsElement.textContent = "Welcome! Let us know if you have any questions.";
-}
-else if ((daysSinceLastVisit > 0) && (daysSinceLastVisit < 1)){
-    visitsElement.textContent = "Back so soon! Awesome!";
-}
-else{
-    daysSinceLastVisit = Math.floor(daysSinceLastVisit);
-    visitsElement.textContent = `You last visited ${daysSinceLastVisit} days ago`
+const getLastVisit = () => JSON.parse(localStorage.getItem('lastVisit'))
+const storeLastVisit = date => {
+  localStorage.setItem('lastVisit', JSON.stringify(date))
 }
 
-localStorage.setItem('lastVisitLocal', today);
+const setMessage = dateDiff => (dateDiff === 0) ? 'Welcome! Let us know if you have any questions.' : (dateDiff < 1) ? 'Back so soon! Awesome!' : `You last visited ${dateDiff.toFixed(0)} days ago.`
+const diffCalc = date => {
+  const msToDays = 24 * 60 * 60 * 1000
+  const lastVisit = getLastVisit() || Date.now()
+  return (lastVisit - date) / msToDays
+}
 
+function displayMessage(date) {
+  const message = setMessage(diffCalc(date))
+
+  messageElement.textContent = message
+  storeLastVisit(date)
+}
+
+const today = Date.now()
+
+displayMessage(today)
