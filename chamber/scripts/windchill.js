9ba3570a49063134e86e548e92c0fd7c2e84c1fd
soliverson <stacy.oliverson@gmail.com>
2024-02-24 17:04:09 -0700

    update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const windChillResult = document.getElementById('windChillResult');
 
     if (temperature <= 50 && windSpeed > 3.0) {
-        const windchillIndex = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+        const windchillIndex = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
         windChillResult.textContent = `Wind Chill: ${windchillIndex.toFixed(2)}°F`;
     } else {
         windChillResult.textContent = 'Wind Chill: N/A';
